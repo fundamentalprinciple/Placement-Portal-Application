873e2df04e6b04ff332c4107261aa6719636bd39
fundamentalprinciple <princedixit931@gmail.com>
2026-09-19 18:53:03 +0800

    prepare backend for production deployment

--- backend/application/config.py
+++ backend/application/config.py
@@ -1,23 +1,42 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
+
 load_dotenv()
+
 
 class Config():
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     SQLITE_DB_DIR = os.path.join(basedir, "../db_directory")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///"+os.path.join(SQLITE_DB_DIR, "testdb.sqlite3")
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(SQLITE_DB_DIR, "testdb.sqlite3")
+    )
 
     SECRET_KEY = os.getenv('SECRET_KEY')
     SECURITY_PASSWORD_SALT = os.getenv('SECURITY_PASSWORD_SALT')
-    
-    CELERY_BROKER_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-    CELERY_RESULT_BACKEND = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-    
-    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.mailtrap.io")
-    MAIL_PORT = int(os.getenv("MAIL_PORT", 2525))
+
+    CELERY_BROKER_URL = os.getenv(
+        "REDIS_URL",
+        "redis://localhost:6379/0"
+    )
+
+    CELERY_RESULT_BACKEND = os.getenv(
+        "REDIS_URL",
+        "redis://localhost:6379/0"
+    )
+
+    MAIL_SERVER = os.getenv(
+        "MAIL_SERVER",
+        "smtp.mailtrap.io"
+    )
+
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 2525))
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")    
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
